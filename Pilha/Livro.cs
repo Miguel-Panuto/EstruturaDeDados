eafcabb1a9583a2e02b4e72c12632d35170b5117
Miguel Panuto <miguel.panuto@live.com>
2019-08-18 14:26:25 -0300

    Codigo finalizado

---
+++ Pilha/Livro.cs
@@ -0,0 +1,19 @@
+﻿namespace Pilha
+{
+	class Livro
+	{
+		private string Nome;
+		private int Id;
+
+		public Livro(string nome, int id)
+		{
+			Nome = nome;
+			Id = id;
+		}
+
+		public override string ToString()
+		{
+			return "O livro " + Nome + " estará na #" + Id + " posição.";
+		}
+	}
+}
--- Pilha/Program.cs
+++ Pilha/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*Criado por: Gabriel Pasqualini e Victor Reis
+ * 
+ * 18/08/2019 às 14:00
+ */
+
+using System;
 using System.Collections;
 
 namespace Pilha
@@ -8,33 +13,37 @@ namespace Pilha
 		static void Main(string[] args)
 		{
 			Stack myStacks = new Stack(); //Criação da pilha
+			int n = 1; //cria a variavel de Id
 
 			while (true) //Repetirá o loop até que a string seja nula ou em branco
 			{
+				Console.WriteLine("Coloque o nome do Livro (Espaço em branco nulo para encerrar a entrada de dados.)");
 				string myString = Console.ReadLine(); //Pega o que foi escrito no console
-				if (String.IsNullOrWhiteSpace(myString))
+
+				if (String.IsNullOrWhiteSpace(myString)) //Cancela o lopping caso a string seja nula ou somente espaços em branco
 				{
 					break;
 				}
 
-				myStacks.Push(myString); // Adiciona elementos na pilha
+				myStacks.Push(new Livro(myString, n)); // Adiciona elementos na pilha
+				n++;
 
 				Console.Clear();
 			}
 
 			Console.Clear();
 
-			Console.WriteLine("\nNumero de elementos: " + myStacks.Count + "\n"); // Apenas para mostrar quantos elementos há na pilha
+			Console.WriteLine("\nQuantidade de livros na pilha: " + myStacks.Count + "\n"); // Apenas para mostrar quantos elementos há na pilha
 
 			while (myStacks.Count > 0)
 			{
 				Console.WriteLine(myStacks.Pop());
 
-				//pop exclui os elementos da fila e retorna a string do último elemento adicionado
+				//Pop exclui os elementos da fila e retorna a string do último elemento adicionado
 
 			}
 
-			Console.WriteLine("\nNumero de elementos: " + myStacks.Count + "\n");
+			Console.WriteLine("Não há mais nenhum livro.");
 		}
 	}
 }
